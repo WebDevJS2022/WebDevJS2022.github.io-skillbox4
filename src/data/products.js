292b04810b85201ba72df9a35486d6e121fe8dd3
WeDev <webdevjs2022@gmail.com>
2023-03-30 08:36:07 +0500

    атрибут key

--- src/data/products.js
+++ src/data/products.js
@@ -1,60 +1,72 @@
 export default [
     {
+        id: 1,
         title: 'T-shirt',
         price: 3690,
         image: '/img/t-shirt1.jpg'
     },
     {
+        id: 2,
         title: 'T-shirt with print',
         price: 5660,
         image: '/img/t-shirt2.jpg'
     },
     {
+        id: 3,
         title: 'T-shirt with V-neck',
         price: 21790,
         image: '/img/t-shirt3.jpg'
     },
     {
+        id: 4,
         title: 'Skirt',
         price: 24690,
         image: '/img/skirt1.jpg'
     },
     {
+        id: 5,
         title: 'Skirt',
         price: 14960,
         image: '/img/skirt2.jpg'
     },
     {
+        id: 6,
         title: 'Skirt',
         price: 8960,
         image: '/img/skirt3.jpg'
     },
     {
+        id: 7,
         title: 'Blouse',
         price: 39900,
         image: '/img/blouse1.jpg'
     },
     {
+        id: 8,
         title: 'Blouse',
         price: 34900,
         image: '/img/blouse2.jpg'
     },
     {
+        id: 9,
         title: 'Blouse',
         price: 4990,
         image: '/img/blouse3.jpg'
     },
     {
+        id: 10,
         title: 'Pants',
         price: 6690,
         image: '/img/pants1.jpg'
     },
     {
+        id: 11,
         title: 'Pants',
         price: 16560,
         image: '/img/pants2.jpg'
     },
     {
+        id: 12,
         title: 'Pants',
         price: 30690,
         image: '/img/pants3.jpg'
