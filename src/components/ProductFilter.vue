39cb563bdbc793d1072a2d83383118a713908d53
WeDev <webdevjs2022@gmail.com>
2023-04-01 10:35:22 +0500

    Название и изображение товара

--- src/components/ProductFilter.vue
+++ src/components/ProductFilter.vue
@@ -39,14 +39,13 @@
           </fieldset>
 
           <fieldset class="form__block">
-            <legend class="form__legend">Объемб в ГБ</legend>
+            <legend class="form__legend">Size</legend>
             <ul class="check-list">
               <li class="check-list__item">
                 <label class="check-list__label">
                   <input class="check-list__check sr-only" type="checkbox" name="volume" value="8" checked>
                   <span class="check-list__desc">
-                    8 
-                    <span>(313)</span>
+                    XS 
                   </span>
                 </label>
               </li>
@@ -54,8 +53,7 @@
                 <label class="check-list__label">
                   <input class="check-list__check sr-only" type="checkbox" name="volume" value="16">
                   <span class="check-list__desc">
-                    16 
-                    <span>(461)</span>
+                    S 
                   </span>
                 </label>
               </li>
@@ -63,8 +61,7 @@
                 <label class="check-list__label">
                   <input class="check-list__check sr-only" type="checkbox" name="volume" value="32">
                   <span class="check-list__desc">
-                    32 
-                    <span>(313)</span>
+                    XM
                   </span>
                 </label>
               </li>
@@ -72,8 +69,7 @@
                 <label class="check-list__label">
                   <input class="check-list__check sr-only" type="checkbox" name="volume" value="64">
                   <span class="check-list__desc">
-                    64 
-                    <span>(313)</span>
+                    M
                   </span>
                 </label>
               </li>
@@ -81,8 +77,7 @@
                 <label class="check-list__label">
                   <input class="check-list__check sr-only" type="checkbox" name="volume" value="128">
                   <span class="check-list__desc">
-                    128 
-                    <span>(313)</span>
+                   L
                   </span>
                 </label>
               </li>
@@ -90,8 +85,7 @@
                 <label class="check-list__label">
                   <input class="check-list__check sr-only" type="checkbox" name="volume" value="264">
                   <span class="check-list__desc">
-                    264 
-                    <span>(313)</span>
+                    XL
                   </span>
                 </label>
               </li>
--- src/pages/ProductPage.vue
+++ src/pages/ProductPage.vue
@@ -23,50 +23,23 @@
         <section class="item">
             <div class="item__pics pics">
                 <div class="pics__wrapper">
-                    <img width="570" height="570" src="img/phone-square.jpg" srcset="img/phone-square@2x.jpg 2x"
-                        alt="Название товара">
+                    <img width="570" height="570" :src="product.image" :alt="product.title">
                 </div>
-                <ul class="pics__list">
-                    <li class="pics__item">
-                        <a href="" class="pics__link pics__link--current">
-                            <img width="98" height="98" src="img/phone-square-1.jpg" srcset="img/phone-square-1@2x.jpg 2x"
-                                alt="Название товара">
-                        </a>
-                    </li>
-                    <li class="pics__item">
-                        <a href="" class="pics__link">
-                            <img width="98" height="98" src="img/phone-square-2.jpg" srcset="img/phone-square-2@2x.jpg 2x"
-                                alt="Название товара">
-                        </a>
-                    </li>
-                    <li class="pics__item">
-                        <a href="" class="pics__link">
-                            <img width="98" height="98" src="img/phone-square-3.jpg" srcset="img/phone-square-3@2x.jpg 2x"
-                                alt="Название товара">
-                        </a>
-                    </li>
-                    <li class="pics__item">
-                        <a class="pics__link" href="#">
-                            <img width="98" height="98" src="img/phone-square-4.jpg" srcset="img/phone-square-4@2x.jpg 2x"
-                                alt="Название товара">
-                        </a>
-                    </li>
-                </ul>
             </div>
 
             <div class="item__info">
-                <span class="item__code">Артикул: 150030</span>
+                <span class="item__code">Артикул: {{ product.id }}</span>
                 <h2 class="item__title">
-                    Смартфон Xiaomi Mi Mix 3 6/128GB
+                    {{ product.title }}
                 </h2>
                 <div class="item__form">
                     <form class="form" action="#" method="POST">
                         <b class="item__price">
-                            18 990 ₽
+                            {{ product.price }} $
                         </b>
 
                         <fieldset class="form__block">
-                            <legend class="form__legend">Цвет:</legend>
+                            <legend class="form__legend">Color:</legend>
                             <ul class="colors">
                                 <li class="colors__item">
                                     <label class="colors__label">
@@ -93,14 +66,14 @@
                         </fieldset>
 
                         <fieldset class="form__block">
-                            <legend class="form__legend">Объемб в ГБ:</legend>
+                            <legend class="form__legend">Size:</legend>
 
                             <ul class="sizes sizes--primery">
                                 <li class="sizes__item">
                                     <label class="sizes__label">
                                         <input class="sizes__radio sr-only" type="radio" name="sizes-item" value="32">
                                         <span class="sizes__value">
-                                            32gb
+                                            S
                                         </span>
                                     </label>
                                 </li>
@@ -108,7 +81,7 @@
                                     <label class="sizes__label">
                                         <input class="sizes__radio sr-only" type="radio" name="sizes-item" value="64">
                                         <span class="sizes__value">
-                                            64gb
+                                            M
                                         </span>
                                     </label>
                                 </li>
@@ -117,7 +90,7 @@
                                         <input class="sizes__radio sr-only" type="radio" name="sizes-item" value="128"
                                             checked>
                                         <span class="sizes__value">
-                                            128gb
+                                            L
                                         </span>
                                     </label>
                                 </li>
@@ -142,7 +115,7 @@
                             </div>
 
                             <button class="button button--primery" type="submit">
-                                В корзину
+                                To cart
                             </button>
                         </div>
                     </form>
